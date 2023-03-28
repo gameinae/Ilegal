39ee789d37ef09d579c269fa4ce57cffb5ae9469
Asad <asad.akhtar@w3villa.com>
2023-03-28 12:58:44 +0530

    config

--- index.js
+++ index.js
@@ -2554,6 +2554,6 @@ app.post("/giveRewardsToUsers", async(req, res) => {
     }
 });
 
-// app.listen(port, () => {
-//   console.log("Express server listening on port %d in %s mode");
-// });
+app.listen(port, () => {
+    console.log("Express server listening on port %d in %s mode");
+});
